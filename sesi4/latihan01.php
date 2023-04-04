d758aee69b911cdfc46b50b48249d20aae000a7d
RIRINDAUD <127808974+RIRINDAUD@users.noreply.github.com>
2023-04-04 12:27:30 +0800

    Add files via upload

---
+++ sesi4/latihan01.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form data</title>
+</head>
+<body>
+
+<?php
+
+    if(isset($_GET["txUSER"])){
+         $usr = $_GET ["txUSER"];
+         if($usr==""){
+             echo "<div><h3 style='color:red;padding:5px;'>User Name belum diisikan</h3></div>";  
+         }
+
+    }
+
+?>
+
+
+    <form action="latihan01.php" method="GET">
+        <div>
+            user name
+            <input type="text" name="txUSER">
+        </div>
+
+        <div>
+            password
+            <input type="password" id="txPASKEY" name="txPASSKEY">
+        </div>
+
+        <div>
+            <button type="submit"> login </button>
+            <a href="daftar.php"> Daftar </a>
+        </div>
+    </form>
+
+
+</body>
+</html>
